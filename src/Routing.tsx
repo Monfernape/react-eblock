561f3eab0f4e8ccc6ed8ab7110177e0b34ca6659
zubaidrasool007 <web.expert1221@gmail.com>
2020-04-15 11:15:32 +0500

    Initial login code

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,41 +1,31 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Login from './components/Auth/Login';
 
 //TODO: IMPLEMENTING AUTH
 const Routing: React.FC = () => {
     return (
         <Router>
-                <Switch>
-                    <Route exact path="/"
-                        component={() => 
-                            <div>
-                                Component ONE
-                            </div>
-                            // <PrivateRoute
-                            //     yes={() => (
-                            //         <div>Default Route</div>
-                            //     )}
-                            //     no={() => (
-                            //         <Redirect to="/login" />
-                            //     )}
-                            // />
-                        } 
-                    />
-                    <Route exact path="/two"
-                        component={() => 
-                            <div>Component TWO</div>
-                            // <PrivateRoute
-                            //     yes={() => (
-                            //         <div>Second Route</div>
-                            //     )}
-                            //     no={() => (
-                            //         <Redirect to="/login" />
-                            //     )}
-                            // />
-                        } 
-                    />
-                    <Redirect to='/' />
-                </Switch>
+            <Switch>
+                <Route exact path="/"
+                    component={Login}
+                />
+                <Route exact path="/two"
+                    component={() =>
+                        <div>Component TWO</div>
+                        // <PrivateRoute
+                        //     yes={() => (
+                        //         <div>Second Route</div>
+                        //     )}
+                        //     no={() => (
+                        //         <Redirect to="/login" />
+                        //     )}
+                        // />
+                    }
+                />
+                <Redirect to='/' />
+            </Switch>
         </Router>
     )
 }
---
+++ src/components/Auth/Login.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import './index.css';
+
+const Login: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
+    const [mainError, setMainError] = useState('');
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('current-user') || '');
+        if (Object.keys(user).length > 0) {
+            // Go to user Dashboard
+        }
+    }, []);
+
+    const checkLogin = (event: any) => {
+        event.preventDefault();
+        if (username.length === 0 || password.length === 0) {
+            setMainError('All the fields are required!');
+            return;
+        } else if (username.length > 0 && password.length > 0) {
+            setMainError('');
+            const users = JSON.parse(localStorage.getItem('users') || '');
+            const user = users.find((x: any) => ((x.username === username || x.email === username) && x.password === password));
+            if (user) {
+                setUser(user);
+                localStorage.setItem('current-user', JSON.stringify(user));
+                setUsername('');
+                setPassword('');
+            } else {
+                setMainError('Incorrect Username or Password!')
+            }
+        }
+    }
+
+    return (
+        <div>
+            {mainError.length > 0 && <span className="error">{mainError}</span>}
+            <form onSubmit={e => checkLogin(e)}>
+                <div>
+                    <TextField id="username" label="Username/Email" defaultValue=""
+                        onChange={e => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    <TextField id="password" label="Password" type="password" defaultValue=""
+                        onChange={e => setPassword(e.target.value)} />
+                </div>
+                <div>
+                    <input type="submit" value="Login" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
