f5eb47ee3fa6d2589eb83793e391d57990cccf77
Muhammad Zubaid Rasool <63468411+zubaidrasool007@users.noreply.github.com>
2020-05-21 12:48:00 +0500

    Removed the folders to rename

--- src/Config/Constants.ts
+++
@@ -1,8 +0,0 @@
-import {MaintenanceType} from "../models/Car";
-
-export const MaintenanceTypes : MaintenanceType[] = [
-    { id: "1", option: "Tuning" },
-    { id: "2", option: "Inspection" },
-    { id: "3", option: "Brakes Service" },
-    { id: "4", option: "Parts Replacement" }
-];
--- src/Models/Map.ts
+++
@@ -1,9 +0,0 @@
-export interface IMap {
-    zoom: number
-    position: {lat: number, lng: number}
-}
-
-export class UserMap implements IMap {
-    zoom = 13
-    position = {lat: 0, lng: 0}
-}
