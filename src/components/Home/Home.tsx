b29fcc4a6bb22374bd1cf7af473e935cfecffa2f
Muhammad Zubaid Rasool <63468411+zubaidrasool007@users.noreply.github.com>
2020-05-21 13:16:31 +0500

    Create Home.tsx

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Select, MenuItem, TextField, Grid, Checkbox, FormControlLabel } from "@material-ui/core";
+
+const HomeComponent = () => {
+    return (
+        <Grid>
+            
+        </Grid>
+    )
+}
