e32fa5405a31aaec1aa074fe7788119d230a68ce
Monfernape <engr.usmankhalil@gmail.com>
2020-05-21 12:44:57 +0500

    Fixed map layout and design.

--- src/shared/components/MapComponent.tsx
+++ src/shared/components/MapComponent.tsx
@@ -3,15 +3,16 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import { UserMap } from '../../models/Map'
 
 export const MapComponent: React.FC<UserMap> = ({ position, zoom }) => {
+  console.log("Position", position)
   return (
-    <Map center={position} zoom={zoom}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      />
-      <Marker position={position}>
-        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-      </Marker>
-    </Map>
+      <Map center={[position.lat, position.lng]} zoom={zoom} style={{width: '100%',height: '100vh'}}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        <Marker position={[position.lat, position.lng]}>
+          <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+        </Marker>
+      </Map>
   )
 }
